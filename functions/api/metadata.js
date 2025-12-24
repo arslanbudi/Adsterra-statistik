c7ea2786971b1ff912a4cbc4ccdc342db173053b
arslanbudi <148300476+arslanbudi@users.noreply.github.com>
2025-12-25 04:16:43 +0700

    Update metadata.js

--- functions/api/metadata.js
+++ functions/api/metadata.js
@@ -1,13 +1,12 @@
-// functions/api/metadata.js
 export async function onRequest(context) {
   const API_KEY = context.env.ADSTERRA_API_KEY;
   if (!API_KEY) return new Response(JSON.stringify({ error: "API Key missing" }), { status: 500 });
 
   try {
-    // Limit 2000 agar semua nama terambil
+    // UPDATE: Naikkan limit ke 10.000 agar semua Direct Link terambil
     const [resPlacements, resDomains] = await Promise.all([
-      fetch(`https://api3.adsterratools.com/publisher/placements.json?limit=2000`, { headers: { "X-API-Key": API_KEY } }),
-      fetch(`https://api3.adsterratools.com/publisher/domains.json?limit=2000`, { headers: { "X-API-Key": API_KEY } })
+      fetch(`https://api3.adsterratools.com/publisher/placements.json?limit=10000`, { headers: { "X-API-Key": API_KEY } }),
+      fetch(`https://api3.adsterratools.com/publisher/domains.json?limit=10000`, { headers: { "X-API-Key": API_KEY } })
     ]);
 
     const jsonPlacements = await resPlacements.json();
@@ -19,6 +18,7 @@ export async function onRequest(context) {
     };
 
     return new Response(JSON.stringify(result), { headers: { "Content-Type": "application/json" } });
+
   } catch (err) {
     return new Response(JSON.stringify({ error: err.message }), { status: 500 });
   }
